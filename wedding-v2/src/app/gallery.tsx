28378cb2160118590385ba48eaaefc0c76c2d179
Matthew <matthew.herradura@gmail.com>
2024-09-22 17:07:40 -0700

    preloading image

--- wedding-v2/src/app/gallery.tsx
+++ wedding-v2/src/app/gallery.tsx
@@ -33,6 +33,13 @@ const Gallery: React.FC<GalleryProps> = ({}) => {
     { img: landing4 }
   ];
 
+  useEffect(() => {
+    gallery.forEach((image) => {
+      const img = new window.Image();
+      img.src = image.img.src;  // Preload the actual image src
+    });
+  }, [gallery]);
+
   const progress = useTransform(timer, [0, timerEnd], [0, 1])
   const [curr, setCurr] = useState(0);
   const [next, setNext] = useState(curr + 1); 
@@ -179,12 +186,12 @@ const Gallery: React.FC<GalleryProps> = ({}) => {
   const prevClick = () => { // handle prev click functions
     setNext(curr);
 
-    textAnimation.start("exit").then(() =>{
-      setDirection("prev");
-      setNextDisplay(true); 
-      setTransition("animatePrev");
-      setCurr(prev);
-    });
+    textAnimation.start("exit")
+    setDirection("prev");
+    setNextDisplay(true); 
+    setTransition("animatePrev");
+    setCurr(prev);
+    
     timer.set(0);
     setTimerEnable(false);
   }
