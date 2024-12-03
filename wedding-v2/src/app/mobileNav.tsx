0fe3cde9497111c76766970efc0c7835593ae789
Matthew <matthew.herradura@gmail.com>
2024-12-02 19:01:51 -0800

    changing nav colors

--- wedding-v2/src/app/mobileNav.tsx
+++ wedding-v2/src/app/mobileNav.tsx
@@ -114,16 +114,16 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
     if (downAnimation == true) {
       setIconOpacity(1);
       controls.start({
-        color: 'black',
-        backgroundColor: 'rgba(245, 245, 245, 1)',
+        color: '#486A51',
+        backgroundColor: 'rgba(250, 251, 247, 1)',
         boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.2)',
         transition: { duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] }
       })
     } else {
       setIconOpacity(0);
       controls.start({
-        color: pathname != '/faq' ? 'white' : 'black',
-        backgroundColor: 'rgba(245, 245, 245, 0)',
+        color: pathname != '/faq' ? 'white' : '#486A51',
+        backgroundColor: 'rgba(250, 251, 247, 0)',
         boxShadow: '0px -2px 10px rgba(0, 0, 0, 0)',
         transition: { duration: 0.3, ease: [0.43, 0.13, 0.23, 0.96] }
       })
@@ -150,8 +150,8 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
     if (!navDisplay) { // show navbar links
       setNavDisplay(true);
       controls.start({
-        color: 'black',
-        backgroundColor: 'rgba(245, 245, 245, 1)',
+        color: '#486A51',
+        backgroundColor: 'rgba(250, 251, 247, 1)',
         boxShadow: '0px -2px 10px rgba(0, 0, 0, 0)',
         transition: { duration: 0.1, ease: [0.43, 0.13, 0.23, 0.96] }
       })
@@ -170,7 +170,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
       if (scroll <= 0) {
         controls.start({
           color: 'white',
-          backgroundColor: 'rgba(245, 245, 245, 0)',
+          backgroundColor: 'rgba(250, 251, 247, 0)',
           boxShadow: '0px -2px 10px rgba(0, 0, 0, 0)',
           transition: {duration: 0.1, ease: [0.43, 0.13, 0.23, 0.96] }
         })
@@ -194,7 +194,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
         >
           <div className="flex justify-between w-11/12 items-center">
             <motion.h2 
-              className="text-4xl"
+              className="text-4xl text-[#486A51]"
               style={{
                 opacity: iconOpacity
               }}
@@ -204,7 +204,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
               </Link>
             </motion.h2>
             <div className="cursor-pointer" onClick={handleClick}>
-              {navDisplay && <div className='text-white bg-black rounded-full w-8 h-8 flex items-center justify-center border-2'><X size={24}/></div> || <Menu size={30} />}
+              {navDisplay && <div className='text-white bg-[#486A51] rounded-full w-8 h-8 flex items-center justify-center'><X size={24}/></div> || <Menu size={30} />}
             </div>
           </div>
         </motion.section>
@@ -213,7 +213,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
         >
           {navDisplay &&
             <motion.section
-              className="flex-grow bg-[#f5f5f5] relative z-0"
+              className="flex-grow bg-[#FAFBF7] relative z-0"
               initial={{
                 height: 0
               }}
@@ -239,7 +239,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
                 exit={"exit"}
               >
                 <motion.li 
-                  className="text-black py-4 px-4  text-3xl"
+                  className="text-[#486A51] py-4 px-4  text-3xl"
                   variants={links}
                 >
                   <Link href="/">
@@ -247,7 +247,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
                   </Link>
                 </motion.li>
                 <motion.li 
-                  className="text-black py-4 px-4  text-3xl"
+                  className="text-[#486A51] py-4 px-4  text-3xl"
                   variants={links}
                 >
                   <Link href="/ceremony">
@@ -255,13 +255,13 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
                   </Link>
                 </motion.li>
                 <motion.li 
-                  className="text-black py-4 px-4 text-3xl"
+                  className="text-[#486A51] py-4 px-4 text-3xl"
                   variants={links}
                 >
                   Registry
                 </motion.li>
                 <motion.li 
-                  className="text-black py-4 px-4 text-3xl"
+                  className="text-[#486A51] py-4 px-4 text-3xl"
                   variants={links}
                 >
                   <Link href="/faq">
@@ -269,7 +269,7 @@ const MobileNav: React.FC<MobileNavProps> = ({ scroll, height }) => {
                   </Link>
                 </motion.li>
                 <motion.li 
-                  className="text-black px-4 py-2 text-3xl bg-black text-white"
+                  className="text-[#FAFBF7] px-4 py-2 text-3xl bg-[#486A51] text-white"
                   variants={links}
                 >
                   <Link href="/rsvp">
